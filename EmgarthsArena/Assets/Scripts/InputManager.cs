f7e83199c462c68ebf3618f0844ed2b2fe39a9ae
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2018-11-21 13:30:28 +0100

    no message

--- EmgarthsArena/Assets/Scripts/InputManager.cs
+++ EmgarthsArena/Assets/Scripts/InputManager.cs
@@ -17,13 +17,7 @@ public class InputManager {
     public InputManager()
     {
         //Initialize all the button layouts here.
-        spellCastButton1 = "JoyStickLeftBumper";
-        spellCastButton2 = "JoyStickRightBumper";
-        moveAxisHorizontal = "LeftJoyStickHorizontal";
-        moveAxisVertical = "LeftJoyStickVertical";
-        lookAxisHorizontal = "RightJoyStickHorizontal";
-        lookAxisVertical = "RightJoyStickVertical";
-        jumpButton = "XButton";
+     
     }
 
     public string GetMoveAxisHorizontal()
