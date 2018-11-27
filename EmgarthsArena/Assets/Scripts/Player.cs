c1fc2e2077bb0b1fc76b9c5c5b2dbdb9b44833f6
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2018-11-27 13:04:36 +0100

    Casting particles.

--- EmgarthsArena/Assets/Scripts/Player.cs
+++ EmgarthsArena/Assets/Scripts/Player.cs
@@ -22,6 +22,8 @@ public class Player : MovingObject {
     private bool _changingElement2 = false;
     private bool _disableMovement = false;
     private bool _castingSpell = false;
+    public ParticleSystem _castingParticle;
+    public ParticleSystem _jumpParticle;
 
     private bool _grounded = false;
     private bool _usedDoubleJump = false;
@@ -53,7 +55,9 @@ public class Player : MovingObject {
         _myMagicCircleRight.gameObject.SetActive(false);
         _myCirclePointer = transform.Find("UI Elements").Find("PointerPivot");
         _myCirclePointer.gameObject.SetActive(false);
-        
+        _jumpParticle = GetComponent<ParticleSystem>();
+        _castingParticle = transform.Find("Casting").GetComponent<ParticleSystem>();
+        Debug.Log(_jumpParticle);
     }
 
     // Update is called once per frame
@@ -173,13 +177,15 @@ public class Player : MovingObject {
         {
             if (_grounded)
             {
-                GetComponent<ParticleSystem>().Play();
+                _jumpParticle.Play();
+                _castingParticle.Stop();
                 _rb.AddForce(new Vector2(0, Glob.jumpHeight), ForceMode2D.Impulse);
                 _jumpTime = Time.time;
             }
             else if (!_usedDoubleJump)
             {
-                GetComponent<ParticleSystem>().Play();
+                _jumpParticle.Play();
+                _castingParticle.Stop();
                 _rb.velocity = new Vector2(_rb.velocity.x, 0);
                 _rb.AddForce(new Vector2(0, Glob.jumpDoubleHeight), ForceMode2D.Impulse);
                 _usedDoubleJump = true;
@@ -231,6 +237,7 @@ public class Player : MovingObject {
     {
         yield return new WaitForSeconds(pSpell.GetCastTime());
         _disableMovement = false;
+        _castingParticle.Stop();
         _castingSpell = false;
         Vector3 position = this.transform.position + new Vector3(pInput.x * Glob.spellOffset, pInput.y * Glob.spellOffset, 0);
         Spell launchedspelltest = Instantiate(pSpell, position, new Quaternion());
@@ -250,7 +257,8 @@ public class Player : MovingObject {
 
     //This is for loading animations etc.
     private void HandleCastingBehaviour()
-    {
+    {        
+        _castingParticle.Play();
         _castingSpell = true;
         _disableMovement = true;
     }
